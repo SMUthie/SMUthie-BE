763f06da783122af9958ce1502f6046c8778ccd7
newoldname <thjbjs@outlook.com>
2023-09-13 16:02:01 +0900

    [Fix] 배포 후 dotenv 적용 오류 수정

--- config/baseResponseStatus.js
+++ config/baseResponseStatus.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Success
-  SUCCESS: { isSuccess: true, code: 1000, message: '성공' },
+  SUCCESS: { isSuccess: true, code: 1000, message: '성공!' },
 
   // Common
   TOKEN_EMPTY: {
