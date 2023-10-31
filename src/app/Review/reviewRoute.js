f431da26c530cadca8c065cdba4e1d80d08b04cf
JSH99 <tjgus9966@naver.com>
2023-10-31 16:44:54 +0900

    [Feat] 리뷰글 조회 및 등록 API 구현

---
+++ src/app/Review/reviewRoute.js
@@ -0,0 +1,13 @@
+module.exports = function(app){
+    const review = require('./reviewController');
+
+    // 1. 가게의 리뷰글 전체 조회 API
+    app.get('/app/board/review/:storeIdx', review.getReviewAll); 
+
+    // 2. 가게의 리뷰글 생성 API (Create)
+    app.post('/app/board/review/:storeIdx', review.postReview); 
+
+    // 3. 리뷰글 상세 조회 API
+    app.get('/app/board/review/detail/:reviewIdx', review.getReview); 
+
+};
